fb91a68b70f534ffb206ffc85a1b238fb4d75015
Marcus Bernhard <Marcus.Bernhard@outlook.com>
2018-10-15 13:27:34 +0200

    ListView funkar

--- ProjectStore/MyForm.cs
+++ ProjectStore/MyForm.cs
@@ -5,11 +5,66 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using System.IO;
 
 namespace ProjectStore
 {
-    class MyForm : Form
+    class Products
     {
+        public string name;
+        public int price;
 
     }
+
+    class MyForm : Form
+    {
+        
+        public Products[] merc;
+
+        public MyForm()
+        {
+            string[] items = File.ReadAllLines("Text1.csv");
+
+            merc = new Products[items.Length];
+
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                string[] stock = items[i].Split(',');
+                merc[i] = new Products { name = stock[0], price = int.Parse(stock[1]),};
+
+            }
+
+            
+
+            TableLayoutPanel table = new TableLayoutPanel
+            {
+                ColumnCount = 1,
+                RowCount = 2,
+                Dock = DockStyle.Fill
+            };
+            Controls.Add(table);
+
+            ListView listView1 = new ListView();
+            {
+
+
+
+            }
+            table.Controls.Add(listView1);
+
+        }
+            private void CreatePicture(string path)
+            {
+                PictureBox box1 = new PictureBox
+                {
+                    Image = Image.FromFile(path),
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    Width = 150,
+                    Height = 150
+                };
+
+            }
+    }
 }
+
