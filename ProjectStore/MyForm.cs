a57879cdec56332bdd2d0b679d414fcf2299dae2
Marcus Bernhard <Marcus.Bernhard@outlook.com>
2018-10-18 13:20:28 +0200

    Alpha efter git problem

--- ProjectStore/MyForm.cs
+++ ProjectStore/MyForm.cs
@@ -24,15 +24,11 @@ namespace ProjectStore
                 Dock = DockStyle.Fill
             };
             Controls.Add(table);
-
-            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
-            table.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
-            table.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
-            table.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
-            table.RowStyles.Add(new RowStyle(SizeType.AutoSize));
-            table.RowStyles.Add(new RowStyle(SizeType.AutoSize));
-            table.RowStyles.Add(new RowStyle(SizeType.Percent, 50));
-            table.RowStyles.Add(new RowStyle(SizeType.Percent, 50));
+            for (int i = 0; i < 4; i++)
+            {
+                table.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+                table.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            }
 
             ListView listView1 = new ListView();
             {
@@ -46,19 +42,52 @@ namespace ProjectStore
                 listview2.Height = 400;
                 listview2.Width = 250;
             }
-            table.Controls.Add(listview2, 3, 1);
+            table.Controls.Add(listview2, 4, 1);
 
-            TextBox discountBox = new TextBox();
+            TextBox rabatt = new TextBox();
             {
-                discountBox.Height = 100;
-                discountBox.Width = 250;
+                rabatt.Height = 100;
+                rabatt.Width = 250;
             }
-            table.Controls.Add(discountBox, 0, 3);
+            table.Controls.Add(rabatt, 0, 4);
 
             Button butt1 = new Button();
             {
-                Text = "Add";
+                butt1.Text = "Add product";
             }
+            table.Controls.Add(butt1, 2, 2);
+
+            Button butt2 = new Button();
+            {
+                butt2.Text = "remove product";
+            }
+            table.Controls.Add(butt2, 2, 3);
+
+            Button butt3 = new Button();
+            {
+                butt3.Text = "Checkout";
+            }
+            table.Controls.Add(butt3, 3, 3);
+
+            Label label1 = new Label();
+            {
+                label1.Text = "Discount code:";
+            }
+            table.Controls.Add(label1, 0, 3);
+
+            RichTextBox box1 = new RichTextBox();
+            {
+                box1.Height = 150;
+                box1.Width = 150;
+            }
+            table.Controls.Add(box1, 2, 1);
+
+            PictureBox pics1 = new PictureBox();
+            {
+                pics1.Height = 250;
+                pics1.Width = 250;
+            }
+            //table.Controls.Add(pics1, 2, 1);
         }
 
     }
